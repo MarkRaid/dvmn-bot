038187d468abfafc2cb022484662eb04d009145d
Mark <akkulov.anton@gmail.com>
2021-02-06 02:53:31 +1000

    glogal refactoring

--- main.py
+++ main.py
@@ -1,54 +1,108 @@
 import requests
-import pprint
 import telegram
-import time
 import os
 import logging
+import zoneinfo
 
-bot = telegram.Bot(token=os.getenv("TELEGRAM_TOKEN"))
+from time import sleep
+from datetime import datetime
+from urllib.parse import urlparse
 
-USER_REVIEWS_URL = "https://dvmn.org/api/long_polling/"
 
-headers = {
-  "Authorization": f"Token {os.getenv('DVMN_API_TOKEN')}"
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+DVMN_API_TOKEN = os.getenv("VMN_API_TOKEN")
+CHAT_ID        = os.getenv("CHAT_ID")
+
+USER_TIME_ZONE = os.getenv("USER_TIME_ZONE")
+START_HOUR     = int(os.getenv("START_HOUR"))
+
+LONG_POLLING_URL = "https://dvmn.org/api/long_polling/"
+AUTHORIZATION_HEADERS = {
+	"Authorization": f"Token {DVMN_API_TOKEN}"
 }
 
 
-logging.basicConfig(level=logging.INFO)
-logging.info('Бот запущен')
+## Messages ##
+START_MESSAGE = "#### DVMN-бот запущен ####"
+
+def get_telegram_report_message(work_title, lesson_url, is_negative):
+	api_url_pr  = urlparse(LONG_POLLING_URL)
+	lesson_path = urlparse(lesson_url).path
+	
+	url_pr = urlparse("")
+	url_pr.scheme = api_url_pr.scheme
+	url_pr.netloc = api_url_pr.netloc
+	url_pr.path   = lesson_path
+
+	url = url_pr.geturl()
+
+	complite = f'✅["{work_title}"]({url}) - сдана✅'
+	mistakes = f'🚫["{work_title}"]({url}) - с ошибкой🚫'
+
+	return (mistakes if is_negative else complite)
+## Messages end ##
 
 
 def get_new_reviews(headers={}, params={}):
-    response = requests.get(USER_REVIEWS_URL, headers=headers, params=params)
-    response.raise_for_status()
-    return response.json()
+	response = requests.get(LONG_POLLING_URL, headers=headers, params=params)
+	response.raise_for_status()
+	return response.json()
 
-timestamp = ""
 
-while True:
-  try:
-    response_data = get_new_reviews(headers=headers, params={"timestamp": timestamp})
-  except requests.exceptions.ReadTimeout:
-    continue
-  except requests.exceptions.ConnectionError:
-    time.sleep(12)
-    continue
+def now_notification_time():
+	now = datetime.now(zoneinfo.ZoneInfo(USER_TIME_ZONE))
+	return (START_HOUR <= now.hour)
 
-  if response_data["status"] == "timeout":
-    timestamp = response_data["timestamp_to_request"]
-  elif response_data["status"] == "found":
-    attempt = response_data["new_attempts"][0]
-    
-    header_text   = f'У вас проверили работу ["{attempt["lesson_title"]}"](https://dvmn.org{attempt["lesson_url"]})"'
-    positive_text = "Преподавателю всё понравилось, можно приступать к следующему уроку"
-    negative_text = "К сожалению, в работе нашлись ошибки"
-    footer_text   = negative_text if attempt["is_negative"] else positive_text
-    message       = header_text + '\n\n' + footer_text
-    
-    bot.send_message(
-      chat_id=os.getenv("CHAT_ID"),
-      text=message,
-      parse_mode=telegram.ParseMode.MARKDOWN
-    )
 
-    timestamp = response_data["last_attempt_timestamp"]
+def start_long_polling_loop():
+	timestamp = ""
+
+	while now_notification_time():
+		try:
+			reviews = get_new_reviews(
+				headers=AUTHORIZATION_HEADERS,
+				params={"timestamp": timestamp},
+			)
+		except (
+			requests.exceptions.ConnectionError,
+			requests.exceptions.HTTPError,
+			requests.exceptions.ReadTimeout
+		):
+			sleep(60)
+			continue
+
+		if reviews["status"]   == "timeout":
+			timestamp = reviews["timestamp_to_request"]
+		elif reviews["status"] == "found":
+			attempts  = reviews["new_attempts"]
+
+			while attempts:
+				attempt = attempts[-1]
+
+				try:
+					bot.send_message(
+						chat_id=CHAT_ID,
+						text=get_telegram_report_message(
+							attempt["lesson_title"],
+							attempt["lesson_url"],
+							attempt["is_negative"]
+						),
+						parse_mode=telegram.ParseMode.MARKDOWN,
+						disable_web_page_preview=True
+					)
+				except telegram.error.TelegramError:
+					raise
+					sleep(60)
+					continue
+				
+				attempts.pop(-1)
+
+			timestamp = reviews["last_attempt_timestamp"]
+
+
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+logging.basicConfig(level=logging.INFO)
+logging.info(START_MESSAGE)
+
+start_long_polling_loop()
