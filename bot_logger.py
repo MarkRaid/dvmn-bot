3b1449cf7805786870818a29bd3244fc33af81d9
Mark <akkulov.anton@gmail.com>
2021-03-12 20:54:22 +1000

    refactoring

--- bot_logger.py
+++ bot_logger.py
@@ -2,13 +2,13 @@ import logging
 
 
 class TelegramHandler(logging.Handler):
-	
-	def __init__(self, bot, chat_id):
-		super().__init__()
-		self.bot = bot
-		self.chat_id = chat_id
+    
+    def __init__(self, bot, chat_id):
+        super().__init__()
+        self.bot = bot
+        self.chat_id = chat_id
 
-	def emit(self, record):	
-		log_entry = self.format(record)
+    def emit(self, record): 
+        log_entry = self.format(record)
 
-		self.bot.send_message(chat_id=self.chat_id, text=log_entry)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import logging
-
-from time import sleep
+import time
 
 import requests
 import telegram
+
+from retrying import retry
 
 from bot_logger import TelegramHandler
 
@@ -17,91 +18,96 @@ logger = logging.getLogger()
 
 # Messages
 def get_start_message():
-	return "#### DVMN-бот начал работать ####"
+    return "# DVMN-бот начал работать #"
 
 
 def get_telegram_report_message(work_title, lesson_url, is_negative):
-	url = DVMN_URL + lesson_url
+    url = DVMN_URL + lesson_url
+    mdLink = f'[{work_title}]({url})'
 
-	complite = f'✅[{work_title}]({url}) - сдана✅'
-	mistakes = f'🚫[{work_title}]({url}) - с ошибкой🚫'
+    complite = f'✅{mdLink} - сдана✅'
+    mistakes = f'🚫{mdLink} - с ошибкой🚫'
 
-	return (mistakes if is_negative else complite)
+    return (mistakes if is_negative else complite)
 # Messages end
 
 
 def get_new_reviews(headers={}, params={}):
-	response = requests.get(LONG_POLLING_URL, headers=headers, params=params)
-	response.raise_for_status()
-	return response.json()
+    response = requests.get(LONG_POLLING_URL, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+def retry_if_telegram_error(exception):
+    return isinstance(exception, telegram.error.TelegramError)
+
+
+@retry(
+    stop_max_attempt_number=20,
+    wait_fixed=1000,
+    retry_on_exception=retry_if_telegram_error
+)
+def send_report_message(bot, telegram_chat_id, attempt):
+    bot.send_message(
+        chat_id=telegram_chat_id,
+        text=get_telegram_report_message(
+            attempt["lesson_title"],
+            attempt["lesson_url"],
+            attempt["is_negative"]
+        ),
+        parse_mode=telegram.ParseMode.MARKDOWN,
+        disable_web_page_preview=True
+    )
 
 
 def start_long_polling_loop(bot, dvmn_api_token, telegram_chat_id):
-	timestamp = ""
+    timestamp = ""
 
-	while True:
-		try:
-			try:
-				reviews = get_new_reviews(
-					headers={"Authorization": f"Token {dvmn_api_token}"},
-					params={"timestamp": timestamp},
-				)
-			except (
-				requests.exceptions.ConnectionError,
-				requests.exceptions.HTTPError,
-			):
-				sleep(60)
-				continue
-			except requests.exceptions.ReadTimeout:
-				continue
+    while True:
+        try:
+            try:
+                reviews = get_new_reviews(
+                    headers={"Authorization": f"Token {dvmn_api_token}"},
+                    params={"timestamp": timestamp},
+                )
+            except (
+                requests.exceptions.ConnectionError,
+                requests.exceptions.HTTPError,
+            ):
+                time.sleep(60)
+                continue
+            except requests.exceptions.ReadTimeout:
+                continue
 
-			resp_status = reviews["status"]
+            resp_status = reviews["status"]
 
-			if resp_status == "timeout":
-				timestamp = reviews["timestamp_to_request"]
-			elif resp_status == "found":
-				for attempt in reversed(reviews["new_attempts"]):
-					for _ in range(10):
-						try:
-							bot.send_message(
-								chat_id=telegram_chat_id,
-								text=get_telegram_report_message(
-									attempt["lesson_title"],
-									attempt["lesson_url"],
-									attempt["is_negative"]
-								),
-								parse_mode=telegram.ParseMode.MARKDOWN,
-								disable_web_page_preview=True
-							)
-							break
-						except telegram.error.TelegramError:
-							sleep(60)
-					else:
-						raise
+            if resp_status == "timeout":
+                timestamp = reviews["timestamp_to_request"]
+            elif resp_status == "found":
+                for attempt in reversed(reviews["new_attempts"]):
+                    send_report_message(bot, telegram_chat_id, attempt)
 
-				timestamp = reviews["last_attempt_timestamp"]
-			else:
-				raise Exception(f"Неизвестный статус ответа сервера: {resp_status}")
-		except:
-			logging.exception("Бот упал с ошибкой:")
+                timestamp = reviews["last_attempt_timestamp"]
+        except:
+            logging.exception("Бот упал с ошибкой:")
 
 
 if __name__ == "__main__":
-	telegram_bot_token  = os.environ["TELEGRAM_BOT_TOKEN"]
-	telegram_chat_id    = os.environ["TELEGRAM_CHAT_ID"]
-	telegram_channel_id = os.environ["TELEGRAM_CHANNEL_ID"]
-	dvmn_api_token      = os.environ["DVMN_API_TOKEN"]
+    telegram_channel_id = os.environ["TELEGRAM_CHANNEL_ID"]
+    telegram_bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
+    telegram_chat_id = os.environ["TELEGRAM_CHAT_ID"]
+    dvmn_api_token = os.environ["DVMN_API_TOKEN"]
 
-	bot = telegram.Bot(token=telegram_bot_token)
+    bot = telegram.Bot(token=telegram_bot_token)
 
-	logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')
-	logger.setLevel(logging.INFO)
-	logger.addHandler(TelegramHandler(bot, telegram_chat_id))
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramHandler(bot, telegram_chat_id))
 
-	logger.info(get_start_message())
+    logger.info(get_start_message())
 
-	start_long_polling_loop(
-		bot,
-		dvmn_api_token,
-		telegram_channel_id
-	)
+    start_long_polling_loop(
+        bot,
+        dvmn_api_token,
+        telegram_channel_id
+    )
